1fbf103572e17aa77bf5947bd88838519bb8d6a3
Camilo Pollero Celis <1146685507@u.icesi.edu.co>
2024-02-07 12:27:47 -0500

    Calculadora sencilla

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Calculadora {
+    public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int X = 8;
+		int Y = 3;
+		int suma = X + Y;
+		int resta = X - Y;
+		int mult = X * Y;
+		double div = X / Y;
+		int mod = X % Y;
+		System.out.println("Los resultados de las operaciones aritmeticas entre 3 y 8 son: \n Suma: " + suma + " \n Resta: " + resta + " \n Multiplicacion: " + mult + "\n Division: " + div + "\n Modulo: " + mod);
+    }
+}
