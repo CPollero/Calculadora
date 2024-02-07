ccd69d0f62e69eb3827bacff8a366c0576fcb8ea
Camilo Pollero Celis <1146685507@u.icesi.edu.co>
2024-02-07 12:53:16 -0500

    Cambios codigo

--- Calculadora.java
+++ Calculadora.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int X = 8;
-		int Y = 3;
+		int X = 8; // Variable
+		int Y = 3; // Variable
 		int suma = X + Y;
 		int resta = X - Y;
 		int mult = X * Y;
